8cc15cf14b632c888ed15388a602a3dcb99a6f89
NABILAFZAL19 <nabilafzal19@gmail.com>
2023-12-16 22:12:31 +0530

    changes

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const PORT = 4000;
+
+const app = express();
+
+app.get("/deployed", (req, res) => {
+  res.send("successfully deployed for ci cd");
+});
+
+app.listen(PORT, () => {
+  console.log(`app listening on post ${PORT}`);
+});
